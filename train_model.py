db459fa625321c71b3736cdbabb81c67d64a4343
ericnsibley <ericnsibley@gmail.com>
2024-06-14 22:13:25 -0500

    fix eval metric

--- train_model.py
+++ train_model.py
@@ -27,8 +27,9 @@ def train_model(df: pd.DataFrame):
     y = df['fraud']
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-    xgb_tree = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
-    xgb_tree.fit(X_train, y_train, eval_metric=auprc_eval, eval_set=[(X_test, y_test)], verbose=True)
+    # xgb_tree = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+    xgb_tree = XGBClassifier(use_label_encoder=False, eval_metric=auprc_eval)
+    xgb_tree.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
 
     y_pred = xgb_tree.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
